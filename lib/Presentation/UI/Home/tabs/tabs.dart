d216de00d5c8c058df5fbaa6b80ea5e72087d4d5
John Safwat <john362sss@gmail.com>
2023-07-24 04:40:08 +0300

    update ui

--- lib/Presentation/UI/Home/tabs/tabs.dart
+++ lib/Presentation/UI/Home/tabs/tabs.dart
@@ -3,6 +3,7 @@ import 'package:chat/Domain/Models/Room/Room.dart';
 import 'package:chat/Presentation/Theme/MyTheme.dart';
 import 'package:chat/Presentation/UI/Home/HomeViewModel.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -38,50 +39,93 @@ class Tabs extends StatelessWidget {
                 return GridView.builder(
                   physics: const BouncingScrollPhysics(),
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 20,
-                    crossAxisSpacing: 20,
-                    childAspectRatio: 0.9
-                  ),
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 20,
+                      crossAxisSpacing: 20,
+                      childAspectRatio: 0.77),
                   padding: const EdgeInsets.all(20),
                   itemCount: roomsList.length,
-                  itemBuilder: (context, index) => Container(
-                    padding: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                              color: MyTheme.black.withOpacity(0.3),
-                              blurRadius: 10,
-                              offset: const Offset(0, 5))
-                        ],
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.white),
-                    child: InkWell(
-                      onTap: () {
-                        goToJoinRoomScreen(roomsList[index]);
-                      },
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Image.asset(
-                            "assets/icons/${roomsList[index].category}.png",
-                            height: 80,
+                  itemBuilder: (context, index) => InkWell(
+                    onTap: () {
+                      goToJoinRoomScreen(roomsList[index]);
+                    },
+                    child: Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                    color: MyTheme.black.withOpacity(0.3),
+                                    blurRadius: 10,
+                                    offset: const Offset(0, 5))
+                              ],
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.white),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                "assets/icons/${roomsList[index].category}.png",
+                                height: 80,
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                roomsList[index].name,
+                                textAlign: TextAlign.center,
+                                maxLines:3,
+                                overflow: TextOverflow.ellipsis,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .displayMedium!
+                                    .copyWith(fontWeight: FontWeight.w500),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              const SizedBox(height: 20,),
+                            ],
                           ),
-                          const SizedBox(height: 10,),
-                          Text(
-                            roomsList[index].name,
-                            textAlign: TextAlign.center,
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            style: Theme.of(context).textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.w500),
+                        ),
+                        Container(
+                          margin:const EdgeInsets.all(0),
+                          padding: const EdgeInsets.symmetric(horizontal: 15 , vertical: 10),
+                          decoration: const BoxDecoration(
+                            color: MyTheme.blue,
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(20),
+                              bottomRight: Radius.circular(20),
+                            ),
                           ),
-                          const SizedBox(height: 10,),
-                          Text(
-                            "${roomsList[index].numberOfMembers.toString()} Member",
-                            style: Theme.of(context).textTheme.displaySmall,
+                          child: Row(
+                            children: [
+                              roomsList[index].type == 'Public'
+                                  ? const Icon(
+                                EvaIcons.peopleOutline,
+                                color: MyTheme.white,
+                              )
+                                  : const Icon(
+                                EvaIcons.lockOutline,
+                                color: MyTheme.white,
+                              ),
+                              Expanded(
+                                child: Text(
+                                  "${roomsList[index].numberOfMembers.toString()} Member",
+                                  textAlign: TextAlign.center,
+                                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                              ),
+                            ],
                           )
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 );
